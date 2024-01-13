198105262f0b49e982dd83508dd2791119fadd98
aastha-cse <guptaaastha824@gmail.com>
2024-01-13 14:52:33 +0530

    added social links and resume to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 import { Container, Row, Col } from "react-bootstrap";
 import logo from "../assets/img/logo.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon2 from "../assets/img/nav-icon2.png";
+import navIcon3 from "../assets/img/nav-icon3.png";
 
 export const Footer = () => {
   return (
@@ -14,9 +14,9 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="https://www.linkedin.com/in/aastha-gupta-507380229/"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="https://www.instagram.com/aastha_gupta18/"><img src={navIcon3} alt="Icon" /></a>
+            <a href="https://www.linkedin.com/in/aastha-gupta-507380229/"target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="" /></a>
+                <a href="https://github.com/aastha-cse"target="_blank" rel="noopener noreferrer"><img src={navIcon2} alt=""/></a>
+                <a href="https://drive.google.com/file/d/1qGtoQDsB1DMjikrdcAuQQYovXn9UKcXX/view?usp=sharing" target="_blank" rel="noopener noreferrer"><img src={navIcon3} alt="" /></a>
             </div>
             <p>Email : guptaaastha824@gmail.com</p>
           </Col>
